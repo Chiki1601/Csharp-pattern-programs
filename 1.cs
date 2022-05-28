a99e8646acec39861767ca74150f45ef6dafc8b3
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 17:15:51 +0530

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,22 @@
+// Pattern Program 1
+using System;
+class PatternProg1 {
+  static void Main() {
+    for(int i =1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            Console.Write("*");
+        }
+    Console.WriteLine();
+    }
+  }
+}
+
+/*
+
+*****
+*****
+*****
+*****
+*****
+
+*/
